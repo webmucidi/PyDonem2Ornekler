093f71dfc2f0ba6a06f347cff7797689dead5e94
WebMucidi <fatihcelik1907@gmail.com>
2023-03-15 15:33:58 +0300

    first commit

---
+++ eposta.py
@@ -0,0 +1,22 @@
+class Uye:
+    def __init__(self,eposta, parola):
+        self.eposta = eposta
+        self.parola = parola
+
+uye1 = Uye("fatihcelik@icloud.com", "fatih123")
+uye2 = Uye("ahmetdemir@icloud.com", "ironman123")
+uye3 = Uye("aysebakir@icloud.com", "prenses123")
+uye4 = Uye("mustafasari@icloud.com", "sari123")
+uye5 = Uye("bedirhanSapkali@icloud.com", "kel123")
+
+print(self.eposta)
+
+girilenEPosta = input("Lütfen E-Posta adresinizi giriniz...")
+if ((girilenEPosta == uye1.eposta) or (girilenEPosta == uye2.eposta) or (girilenEPosta == uye3.eposta)):
+    girilenParola = input("Lütfen parolanızı giriniz...")
+    if (girilenParola == uye1.parola):
+        print("Giriş başarılı!")
+    else:
+        print("Girdiğiniz parola yanlış!")
+else:
+  print("Girdiğiniz E-Posta adresi yanlış...")
